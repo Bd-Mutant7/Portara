bef7bc4ae1a0f37db0a51e61d7eda7496724a339
P_Kariuki007 <spicezke@gmail.com>
2026-02-11 13:05:08 +0300

    Update translations.ts

--- app/routes/translations.ts
+++ app/routes/translations.ts
@@ -3,7 +3,7 @@
  * =====================
  * Switch translations
  *
- * @contributors: Henry [@henryjw] <3443648+henryjw@users.noreply.github.com>
+ * @contributors: P_Kariuki007 [@bd-mutant7] <182138871+bd-mutant7@users.noreply.github.com>
  *
  * @license: MIT License
  *
